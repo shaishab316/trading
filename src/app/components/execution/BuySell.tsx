90b6ea39492cf09903381d43ae91199cde31720d
shaishabcoding <shaishabchandrashil@gmail.com>
2025-07-25 10:12:04 +0600

    feat: Enhance BuySell component with slider keyboard control and UI improvements
    This commit introduces several enhancements to the BuySell component:
    
    - Added keyboard control to the BreakpointSlider for improved accessibility and usability. The slider can now be controlled using number keys 1-9 and arrow keys.
    - Added hover state to BuySell component to trigger keyboard event listeners.
    - Improved styling for UI elements of BuySell.

--- src/app/components/execution/BuySell.tsx
+++ src/app/components/execution/BuySell.tsx
@@ -1,13 +1,14 @@
-import { FaMinus, FaPlus } from "react-icons/fa";
-import BreakpointSlider from "../ui/BreakpointSlider";
-import Tab2 from "../ui/Tab2";
-import Tab from "../ui/Tab";
-import { useState, type FormEventHandler } from "react";
-import toast from "react-hot-toast";
-import sleep from "../../../utils/sleep";
+import { FaMinus, FaPlus } from 'react-icons/fa';
+import BreakpointSlider from '../ui/BreakpointSlider';
+import Tab2 from '../ui/Tab2';
+import Tab from '../ui/Tab';
+import { useState, type FormEventHandler } from 'react';
+import toast from 'react-hot-toast';
+import sleep from '../../../utils/sleep';
 
 export default function BuySell() {
-	const [tab, setTab] = useState("Buy");
+	const [tab, setTab] = useState('Buy');
+	const [hovered, setHovered] = useState(false);
 
 	const handleSubmit: FormEventHandler = async (e) => {
 		e.preventDefault();
@@ -23,12 +24,14 @@ export default function BuySell() {
 
 	return (
 		<form
+			onMouseEnter={() => setHovered(true)}
+			onMouseLeave={() => setHovered(false)}
 			onSubmit={handleSubmit}
-			className="p-6 grow border border-gray-500 rounded-md backdrop-blur-md bg-black/20"
+			className='p-6 grow border border-gray-500 rounded-md backdrop-blur-md bg-black/20'
 		>
-			<div className="flex flex-wrap gap-4 items-center justify-between">
+			<div className='flex flex-wrap gap-4 items-center justify-between'>
 				<Tab2
-					data={["Buy", "Sell"].map((v) => ({
+					data={['Buy', 'Sell'].map((v) => ({
 						children: v,
 						value: v,
 					}))}
@@ -37,109 +40,109 @@ export default function BuySell() {
 				/>
 
 				<Tab
-					data={["Limit", "OCO"].map((v) => ({
+					data={['Limit', 'OCO'].map((v) => ({
 						children: v,
 						value: v,
 					}))}
-					init="OCO"
+					init='OCO'
 					onChange={() => {}}
 				/>
 			</div>
 
-			<div className="mt-4 flex flex-col gap-4">
-				<div className="bg-black/20 backdrop-blur-md rounded-lg relative">
+			<div className='mt-4 flex flex-col gap-4'>
+				<div className='bg-black/20 backdrop-blur-md rounded-lg relative'>
 					<button
-						type="button"
-						className="absolute top-1/2 -translate-y-1/2 left-3"
+						type='button'
+						className='absolute top-1/2 -translate-y-1/2 left-3'
 					>
 						<FaPlus />
 					</button>
 					<input
-						type="number"
-						placeholder="TP Limit (USDT)"
-						className="w-full px-10 py-2 border border-yellow-200/30 rounded-lg focus: outline-0 focus:border-yellow-200/60"
+						type='number'
+						placeholder='TP Limit (USDT)'
+						className='w-full px-10 py-2 border border-yellow-200/30 rounded-lg focus: outline-0 focus:border-yellow-200/60'
 					/>
 					<button
-						type="button"
-						className="absolute top-1/2 -translate-y-1/2 right-3"
+						type='button'
+						className='absolute top-1/2 -translate-y-1/2 right-3'
 					>
 						<FaMinus />
 					</button>
 				</div>
 
-				<div className="bg-black/20 backdrop-blur-md rounded-lg relative">
+				<div className='bg-black/20 backdrop-blur-md rounded-lg relative'>
 					<button
-						type="button"
-						className="absolute top-1/2 -translate-y-1/2 left-3"
+						type='button'
+						className='absolute top-1/2 -translate-y-1/2 left-3'
 					>
 						<FaPlus />
 					</button>
 					<input
-						type="number"
-						placeholder="SL Trigger (USTD)"
-						className="w-full px-10 py-2 border border-yellow-200/30 rounded-lg focus: outline-0 focus:border-yellow-200/60"
+						type='number'
+						placeholder='SL Trigger (USTD)'
+						className='w-full px-10 py-2 border border-yellow-200/30 rounded-lg focus: outline-0 focus:border-yellow-200/60'
 					/>
 					<button
-						type="button"
-						className="absolute top-1/2 -translate-y-1/2 right-3"
+						type='button'
+						className='absolute top-1/2 -translate-y-1/2 right-3'
 					>
 						<FaMinus />
 					</button>
 				</div>
 
-				<div className="flex gap-4">
-					<div className="bg-black/20 backdrop-blur-md rounded-lg relative grow">
+				<div className='flex gap-4'>
+					<div className='bg-black/20 backdrop-blur-md rounded-lg relative grow'>
 						<button
-							type="button"
-							className="absolute top-1/2 -translate-y-1/2 left-3"
+							type='button'
+							className='absolute top-1/2 -translate-y-1/2 left-3'
 						>
 							<FaPlus />
 						</button>
 						<input
-							type="number"
-							placeholder="SL Limit"
-							className="w-full px-10 py-2 border border-yellow-200/30 rounded-lg focus: outline-0 focus:border-yellow-200/60"
+							type='number'
+							placeholder='SL Limit'
+							className='w-full px-10 py-2 border border-yellow-200/30 rounded-lg focus: outline-0 focus:border-yellow-200/60'
 						/>
 						<button
-							type="button"
-							className="absolute top-1/2 -translate-y-1/2 right-3"
+							type='button'
+							className='absolute top-1/2 -translate-y-1/2 right-3'
 						>
 							<FaMinus />
 						</button>
 					</div>
 					<input
-						type="number"
-						placeholder="Limit"
-						className="px-4 py-2 border border-yellow-200/30 rounded-lg focus: outline-0 focus:border-yellow-200/60 bg-black/20 backdrop-blur-md"
+						type='number'
+						placeholder='Limit'
+						className='px-4 py-2 border border-yellow-200/30 rounded-lg focus: outline-0 focus:border-yellow-200/60 bg-black/20 backdrop-blur-md'
 					/>
 				</div>
 
-				<div className="bg-black/20 backdrop-blur-md rounded-lg relative">
+				<div className='bg-black/20 backdrop-blur-md rounded-lg relative'>
 					<button
-						type="button"
-						className="absolute top-1/2 -translate-y-1/2 left-3"
+						type='button'
+						className='absolute top-1/2 -translate-y-1/2 left-3'
 					>
 						<FaPlus />
 					</button>
 					<input
-						type="number"
-						placeholder="Amount"
-						className="w-full px-10 py-2 border border-yellow-200/30 rounded-lg focus: outline-0 focus:border-yellow-200/60"
+						type='number'
+						placeholder='Amount'
+						className='w-full px-10 py-2 border border-yellow-200/30 rounded-lg focus: outline-0 focus:border-yellow-200/60'
 					/>
 					<button
-						type="button"
-						className="absolute top-1/2 -translate-y-1/2 right-3"
+						type='button'
+						className='absolute top-1/2 -translate-y-1/2 right-3'
 					>
 						<FaMinus />
 					</button>
 				</div>
-				<span className="self-end">
-					Max <span className="text-[#2ecc71]">12.12</span>
+				<span className='self-end'>
+					Max <span className='text-[#2ecc71]'>12.12</span>
 				</span>
-				<BreakpointSlider step={7} onChange={() => {}} />
+				<BreakpointSlider step={7} onChange={() => {}} hovered={hovered} />
 				<button
 					className={`${
-						tab === "Buy" ? "bg-[#2ecc71]" : "bg-[#dc143c]"
+						tab === 'Buy' ? 'bg-[#2ecc71]' : 'bg-[#dc143c]'
 					} rounded-lg px-4 py-2 mt-4 text-black`}
 				>
 					{tab}
--- src/app/components/ui/BreakpointSlider.tsx
+++ src/app/components/ui/BreakpointSlider.tsx
@@ -1,14 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps, @typescript-eslint/no-unused-vars */
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 function BreakpointSlider({
 	onChange = (_value) => {},
 	step = 10,
 	init = 5,
+	hovered = false,
 }: {
 	onChange?: (value: number) => void;
 	step?: number;
 	init?: number;
+	hovered?: boolean;
 }) {
 	const [index, setIndex] = useState(init ?? 0);
 
@@ -16,16 +18,45 @@ function BreakpointSlider({
 		onChange(index);
 	}, [index]);
 
+	useEffect(() => {
+		if (!hovered) return;
+
+		const handleKeyDown = ({ key }: KeyboardEvent) => {
+			if (key >= '1' && key <= '9') {
+				const num = parseInt(key);
+				const maxNum = Math.min(9, step + 1);
+				const newValue = Math.min(num - 1, maxNum - 1);
+				setIndex(newValue);
+			} else if (key === 'ArrowLeft') {
+				setIndex((prev) => Math.max(0, prev - 1));
+			} else if (key === 'ArrowRight') {
+				setIndex((prev) => Math.min(step, prev + 1));
+			}
+		};
+
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => window.removeEventListener('keydown', handleKeyDown);
+	}, [hovered, step]);
+
 	return (
-		<div className="w-full relative ">
+		<div
+			aria-label='Breakpoint Slider'
+			aria-valuenow={index}
+			aria-valuemin={0}
+			aria-valuemax={step}
+			role='slider'
+			tabIndex={0}
+			className='w-full flex items-center relative focus-visible:outline-0'
+		>
 			<input
-				type="range"
+				type='range'
 				min={0}
 				max={step}
 				step={1}
 				value={index}
 				onChange={(e) => setIndex(+e.target.valueAsNumber)}
-				className="w-full h-1 rounded-lg appearance-none hide-range"
+				className='w-[calc(100%-32px)] absolute left-[16px] h-1 rounded-lg appearance-none hide-range focus-visible:outline-0'
 				style={{
 					background: `linear-gradient(
             to right,
@@ -36,21 +67,21 @@ function BreakpointSlider({
           )`,
 				}}
 			/>
-			{Array(step + 1)
-				.fill(null)
-				.map((_, i) => (
-					<button
-						type="button"
-						key={i}
-						className={`inline-block absolute w-4 h-4 top-2 rotate-45 rounded-sm ${
-							i <= index ? "bg-[#2ecc71]" : "bg-gray-400"
-						}`}
-						style={{
-							left: `${(i / step) * 98}%`,
-						}}
-						onClick={() => setIndex(i)}
-					></button>
-				))}
+			<div className='absolute flex items-center justify-between w-full focus-visible:outline-0'>
+				{Array(step + 1)
+					.fill(null)
+					.map((_, i) => (
+						<button
+							type='button'
+							key={i}
+							className={`inline-block focus-visible:outline-0 hover:scale-125 hover:rotate-[225deg] focus-visible:scale-125 focus-visible:rotate-[225deg] transition-all duration-500 w-4 h-4 rotate-45 rounded-sm cursor-pointer ${
+								i <= index ? 'bg-[#2ecc71]' : 'bg-gray-400'
+							}`}
+							onClick={() => setIndex(i)}
+							aria-label={`Set to ${i}`}
+						></button>
+					))}
+			</div>
 		</div>
 	);
 }
