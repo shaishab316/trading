92c4b89e9af6da07a6528eaf14f0f6255fad46ec
shaishabcoding <shaishabchandrashil@gmail.com>
2025-07-25 09:32:22 +0600

    feat: Enhance Switch component with hover and keyboard accessibility
    This commit enhances the Switch component by adding:
    
    -   Hover styles for visual feedback.
    -   Keyboard accessibility, allowing users to toggle the switch using Spacebar or Enter keys.
    -   ARIA attributes for improved accessibility.

--- src/app/components/ui/Switch.tsx
+++ src/app/components/ui/Switch.tsx
@@ -1,35 +1,58 @@
 /* eslint-disable react-hooks/exhaustive-deps, @typescript-eslint/no-unused-vars */
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 export default function Switch({
-	openStyle = "",
-	closeStyle = "",
-	className = "",
+	openStyle = '',
+	closeStyle = '',
+	className = '',
 	onToggle = (_value: boolean) => {},
 	init = false,
 }) {
 	const [open, setOpen] = useState(init);
+	const [hovered, setHovered] = useState(false);
 
 	useEffect(() => {
 		onToggle(open);
 	}, [open]);
 
+	useEffect(() => {
+		if (!hovered) return;
+
+		const handleKeyDown = (e: KeyboardEvent) => {
+			if (e.key === ' ' || e.key === 'Spacebar' || e.key === 'Enter') {
+				e.preventDefault();
+				setOpen(!open);
+			}
+		};
+
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => window.removeEventListener('keydown', handleKeyDown);
+	}, [hovered, open]);
+
 	return (
 		<button
-			className={`bg-[#0f616d] rounded-full w-10 h-5 relative ${className}`}
+			className={`bg-[#0f616d] hover:bg-[#074650] focus-visible:bg-[#074650] outline-0 group rounded-full w-10 h-5 relative transition-colors duration-200 ${className}`}
 			onClick={(e) => {
 				e.stopPropagation();
 				setOpen(!open);
 			}}
+			onMouseEnter={() => setHovered(true)}
+			onMouseLeave={() => setHovered(false)}
+			aria-checked={open}
+			role='switch'
+			tabIndex={0}
+			type='button'
+			title='Press space to toggle'
 		>
 			<div
-				className={`w-5 h-5 absolute top-0 rounded-full ${
+				className={`w-5 h-5 group-hover:scale-110 group-focus-visible:scale-110 absolute top-0 rounded-full transition-all duration-200 ${
 					open
-						? "right-0 bg-gradient-to-b from-[#F9DF7B] to-[#B57E10] " +
+						? 'right-0 bg-gradient-to-b from-[#F9DF7B] to-[#B57E10] ' +
 						  openStyle
-						: "left-0 bg-gray-400 " + closeStyle
+						: 'left-0 bg-gray-400 ' + closeStyle
 				}`}
-			></div>
+			/>
 		</button>
 	);
 }
